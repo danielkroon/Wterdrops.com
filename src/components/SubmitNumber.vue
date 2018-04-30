35de5cc3a7bd15c9a2456eefd1d3082d70445529
Daniel Kroon <drg.kroon@gmail.com>
2018-04-30 22:32:31 +0200

    added first draft of diff check

---
+++ src/components/SubmitNumber.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="app">
+   <form @submit.prevent="submitForm">
+     <div>
+       <label for="numberInput"></label>
+       <input id="numberInput" v-model.number="newNumber" type="number" placeholder="Add number" required/>
+     </div>
+     <button type="submit">Submit</button>
+   </form>
+
+   <ul>
+    <li v-for="number in submittedNumbers" :key="number.id">
+       {{ number }}
+     </li>
+   </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  el: '#app',
+  data () {
+    return {
+      // create newNumber array
+      newNumber: {},
+      // store numbers the user filled in.
+      submittedNumbers: [],
+      // last submitted numbers
+      lastSubmittedNumber: [],
+      secondLastSubmittedNumber: []
+    }
+  },
+  methods: {
+    submitForm () {
+      this.submittedNumbers.push(
+        {
+          number: this.newNumber,
+          timestamp: new Date()
+        }
+      )
+      this.newNumber = ''
+
+      this.lastSubmittedNumber = this.submittedNumbers.slice(-1)[0].number
+      this.checkDifference()
+    },
+    checkDifference () {
+      console.log(this.lastSubmittedNumber)
+      if (typeof this.secondLastSubmittedNumber !== 'undefined') {
+        this.secondLastSubmittedNumber = this.submittedNumbers.slice(-2, -1)[0].number
+      }
+      console.log(this.secondLastSubmittedNumber)
+      let difference = Math.abs(this.lastSubmittedNumber - this.secondLastSubmittedNumber)
+      console.log('the differene is ' + difference)
+      if (difference > 10) {
+        console.log('you are using to much')
+      } else {
+        console.log('great job')
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
