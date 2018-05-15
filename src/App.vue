35351f8f76fbc0b329df336de354a05d790ab4f8
Daniel Kroon <drg.kroon@gmail.com>
2018-05-15 21:22:00 +0200

    added navbar with materialize

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,22 @@
 <template>
   <div>
-    <submit-number></submit-number>
+    <navbar />
+    <submit-number />
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar'
 import SubmitNumber from './components/SubmitNumber'
 
 export default {
   components: {
     // Add a reference to the submitNumber component in the components property
-    SubmitNumber
+    SubmitNumber,
+    Navbar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="navbar">
+        <nav class="nav-extend blue darken-2">
+            <div class="nav-content">
+                <router-link to="">
+                    <span class="nav-title">Waterstats</span>
+                </router-link>
+                <ul class="right">
+                    <li>
+                        <a href="" class="waves-effect waves-light btn  blue lighten-1">Add
+                            <router-link to="">
+                                 <i class="material-icons">add</i>
+                            </router-link>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+.navbar .nav-title {
+    padding: 0px 0px 0px 10px;
+}
+</style>
+
