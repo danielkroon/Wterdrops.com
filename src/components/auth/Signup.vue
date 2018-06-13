4a5841a46ca01198b171b5477fe86d3f2e0ea51f
Daniel Kroon <drg.kroon@gmail.com>
2018-06-13 19:42:26 +0200

    added first alias exists check

--- src/components/auth/Signup.vue
+++ src/components/auth/Signup.vue
@@ -14,26 +14,54 @@
         <label for="alias">Alias:</label>
         <input type="text" name="alias" v-model="alias">
       </div>
+      <div class="field">
+        <label for="household">Household size:</label>
+        <input type="number" name="household" v-model="household">
+      </div>
+      <p class="center red-text" v-if="feedback">{{ feedback }}</p>
       <div class="field center">
-        <botton class="btn blue lighten-1">Signup</botton>
+        <button class="btn blue lighten-1">Signup</button>
       </div>
     </form>
   </div>
 </template>
 
 <script>
+import slugify from 'slugify'
+import db from '@/firebase/init'
+
 export default {
   name: 'Signup',
   data () {
     return {
       email: null,
       password: null,
-      alias: null
+      household: null,
+      alias: null,
+      feedback: null,
+      slug: null
     }
   },
   methods: {
     signup () {
-
+      if (this.alias) {
+        this.slug = slugify(this.alias, {
+          replacement: '-',
+          remove: /[$*_+~.()'"!\-:@]/g,
+          lower: true
+        })
+        let ref = db.collection('users').doc(this.slug)
+        ref.get().then(doc => {
+          if (doc.exists) {
+            this.feedback = 'This alias already exists'
+          } else {
+            this.feedback = 'This alias is free to use'
+          }
+        })
+        console.log(this.slug)
+      } else {
+        this.feedback = 'You must enter an alias'
+      }
     }
   }
 }
